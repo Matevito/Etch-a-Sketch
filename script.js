b26dd80b4fc5805344399694632348bf25042c4d
Mateo <madiazt@unal.edu.co>
2020-09-30 16:01:15 -0500

    centered elements in the page

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const grid_len = 6;
+const grid_len = 16;
 const container_len = 500;
 
 const grid_container = document.querySelector("#container");
+
+function create_square (){
+    //create an square div element and return it
+    let square = document.createElement("div")
+    square.style.height = `${(container_len/grid_len)}px`;
+    square.style.width = `${(container_len/grid_len)}px`;
+    square.style.border = "1px solid black";
+    return square;
+}
 
 for (i=0; i < grid_len; i++){
     let column = document.createElement("div");
@@ -9,15 +18,11 @@ for (i=0; i < grid_len; i++){
 
     for (y=0; y < grid_len; y++){
 
-        //this can be a function called: create square*
-        let square = document.createElement("div")
-        square.style.height = `${500/grid_len}px`;
-        square.style.width = `${500/grid_len}px`;
-        square.style.border = "1px solid black";
-
-        //add the square to the column element.
-        column.appendChild(square);
+        //create and append an square for the column
+        let square = create_square ();
+        column.appendChild(square)
     }
     //append the row in the flexbox container
     grid_container.appendChild(column);
 }
+
