58c1c66a22ebeb06b5523f8d4ecec6fac44101d6
Mateo <madiazt@unal.edu.co>
2020-10-01 11:16:22 -0500

    added changable grid size

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 Etch-a-Sketch
 
 This webpage consist of an etch-sketch web simulator using the mouse.
+
+1.todo: random color button.
+2.todo: design an algorithm that takes a random color.
+3.todo: bug that makes that an orange square cannot be painted to black.
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container_len = 500;
-let grid_len = 15;
+let grid_len = 8;
+color_hover = "black_hover"
 
 const grid_container = document.querySelector("#container");
 
@@ -27,17 +28,76 @@ for (i=0; i < grid_len; i++){
 }
 
 //event to paint squares hovering them
-const squares_to_paint = document.querySelectorAll(".square")
+let squares_to_paint = document.querySelectorAll(".square")
 
 squares_to_paint.forEach((square) => {
     square.addEventListener("mouseenter", () => {
-        square.classList.add("hovered_place")
+        square.classList.add(color_hover)
+        console.log(square)
     })
 })
 
-const reset_btn = document.querySelector("#reset_grid");
-console.log(squares_to_paint)
+//reset grid button event  
 
+const reset_btn = document.querySelector("#reset_grid");
+
+    //retire black and orange class from squares
 reset_btn.addEventListener("click", () => {
-   squares_to_paint.forEach((square) => square.classList.remove("hovered_place")
+   squares_to_paint.forEach((square) => square.classList.remove("black_hover")
     )})
+reset_btn.addEventListener("click", () => {
+   squares_to_paint.forEach((square) => square.classList.remove("orange_hover")
+    )})
+
+//todo: resize grid select option
+
+const size_btn = document.getElementById("size_btn");
+size_btn.addEventListener("click", () => {
+    let size_list = document.getElementById("grid_size").value;
+    grid_len = size_list;
+    //recreate grid (1.earase the grid; 2.create a new one)
+
+    //1.
+
+    let full_grid = document.querySelectorAll(".square");
+
+    full_grid.forEach((square) => square.remove());
+
+    //2.
+    for (i=0; i < grid_len; i++){
+        let column = document.createElement("div");
+        column.setAttribute("style", "display: flex; flex-direction: column");
+    
+        for (y=0; y < grid_len; y++){
+            //create and append an square for the column
+            let square = create_square ();
+            column.appendChild(square)
+        }
+        //append the row in the flexbox container
+        grid_container.appendChild(column);
+    }
+
+    //3. take in a variable the new squares nodes and refresh the event
+    let squares_to_paint = document.querySelectorAll(".square");
+    squares_to_paint.forEach((square) => {
+        square.addEventListener("mouseenter", () => {
+            square.classList.add(color_hover)
+            console.log(square)
+        })
+    });
+})
+
+//todo: change hover color:
+
+const black_btn = document.getElementById("black");
+const orange_btn = document.getElementById("orange");
+const random_btn = document.getElementById("random");
+
+black_btn.addEventListener("click", () => {
+    color_hover = "black_hover"
+})
+orange_btn.addEventListener ("click", () =>{
+    color_hover ="orange_hover"
+})
+
+//todo: random color fix
